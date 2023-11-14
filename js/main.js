ad2e085b2c83af1483ff9fc194217e6def07acf8
Emmett Verba <emmettverba@gmail.com>
2023-11-13 17:59:29 -0700

    adds states, constants, and some cached elements

--- js/main.js
+++ js/main.js
@@ -1,12 +1,39 @@
 /*----- constants -----*/
 //deck in format of suit-value, exs: s03 cK hA
+const suits = ['s', 'c', 'd', 'h'];
+const values = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
 const deck = buildDeck();
+const handRanks = [
+    'royal flush',
+    'straight flush',
+    'four of a kind',
+    'full house',
+    'flush',
+    'straight',
+    'three of a kind',
+    'two pair',
+    'pair',
+    'high card'
+];
 
 /*----- state variables -----*/
+let currentRound; //tracks round
+let playerHand; //arr of 2 cards
+let emmettHand;
+let communityCards; //arr of 5 cards
+let shuffledDeck;
+let pot;
+let handWinner; //null, 1 (player), -1 (Emmett)
+let gameWinner; //null, 1 (player), -1 (Emmett)
+let playerBank; 
+let emmettBank;
+let emmettCurrBet;
+let playerCurrBet;
 
 
 /*----- cached elements  -----*/
-
+const disPlayerBet = document.getElementById('player-bet-value');
+const disEmmettBet = document.getElementById('emmett-bet-value');
 
 /*----- event listeners -----*/
 
@@ -14,8 +41,6 @@ const deck = buildDeck();
 /*----- functions -----*/
 
 function buildDeck() {
-    const suits = ['s', 'c', 'd', 'h'];
-    const values = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
     const deck = [];
 
     suits.forEach(function(suit){
