60e9f4929e3a27974d08745de6b8d147b5423d5a
Emmett Verba <emmettverba@gmail.com>
2023-11-14 15:42:03 -0700

    adds the game initiator and the round initiator as well as the render functions

--- js/main.js
+++ js/main.js
@@ -23,8 +23,8 @@ let emmettHand;
 let communityCards; //arr of 5 cards
 let shuffledDeck;
 let pot;
-let handWinner; //null, 1 (player), -1 (Emmett)
-let gameWinner; //null, 1 (player), -1 (Emmett)
+let handWinner; //null, 1 (player), -1 (Emmett), T for tie
+let gameWinner; //null, 1 (player), -1 (Emmett), T for tie
 let playerBank; 
 let emmettBank;
 let emmettCurrBet;
@@ -57,9 +57,148 @@ const disMessage = document.getElementById('message');
 
 initGame();
 function initGame () {
-    //reset community cards
-    communityCards = [null, null, null, null, null];
     shuffledDeck = shuffleDeck();
+    //deal hands
+    playerHand = [shuffledDeck.pop(), shuffledDeck.pop()];
+    emmettHand = [shuffledDeck.pop(), shuffledDeck.pop()];
+    //assign all 5 community cards
+    communityCards = [
+        shuffledDeck.pop(), 
+        shuffledDeck.pop(), 
+        shuffledDeck.pop(),
+        shuffledDeck.pop(),
+        shuffledDeck.pop() 
+    ];
+
+    round = 1;
+
+    if (handWinner === 1) {
+        playerBank = playerBank + pot;
+        pot = 0;
+    } else if (handWinner === -1) {
+        emmettBank = emmettBank + pot;
+        pot = 0;
+    } else {
+        emmettBank = 200;
+        playerBank = 200;
+        pot = 0;
+    }
+
+    playerCurrBet = 0;
+    emmettCurrBet = 0;
+    pot = 0;
+    initRound();
+}
+
+function initRound() {
+    if (round < 5) {
+        pot = playerCurrBet + emmettCurrBet;
+        playerCurrBet = 0;
+        emmettCurrBet = 0;
+        message = 'Your turn!';
+    } else if (round === 5) {
+        handWinner = compareHands();
+        renderEmmettHand();
+        if (handWinner === 1) { //if player wins
+            playerBank = playerBank + pot;
+            pot = 0;
+            if (emmettBank === 0)  {
+                gameWinner = 1;
+                handWinner = null;
+                // console.log('tou beat emmett');
+                message = 'You have beaten Emmett in Poker!!!';
+            } else {
+                message = 'You have won the round! Get ready for the next round!'
+                //console.log('yay');
+                render();
+                setTimeout(initGame, 3000);
+                return;
+            }
+        } else if (handWinner === -1) {
+            emmettBank = emmettBank + pot;
+            pot = 0;
+            if (playerBank === 0) {
+                gameWinner = -1;
+                handWinner = null;
+                message = 'You have lost to Emmett in Poker!!!';
+            } else {
+                message = 'Emmett has won the round! Get ready for the next round!'
+                render();
+                setTimeout(initGame, 3000);
+                return;
+            }
+        } else if (handWinner === 'T') {
+            let potRem = pot % 2;
+            let evenPot = pot - potRem;
+            emmettBank = emmettBank + evenPot/2;
+            playerBank = playerBank + potRem + evenPot/2;
+            pot = 0;
+            message = 'You have tied! The pot has been split.'
+            // console.log(emmettBank)
+            // console.log(playerBank)
+            render();
+            setTimeout(initGame, 3000);
+            return;
+        }
+    }
+    render();
+}
+
+function render () {
+    renderBets();
+    renderBanks();
+    renderPot();
+    renderCommunityCards();
+    renderPlayerHand();
+    renderMessage();
+}
+
+function renderBets() {
+    disPlayerBet.innerText = playerCurrBet.toString();
+    disEmmettBet.innerText = emmettCurrBet.toString();
+}
+
+function renderBanks() {
+    disEmmettBank.innerText = emmettBank.toString();
+    disPlayerBank.innerText = playerBank.toString();
+}
+
+function renderPot() {
+    disPot.innerText = pot.toString();
+}
+
+function renderCommunityCards () {
+    if (round >= 2) {
+        disCommunityCard1.setAttribute('class', 'card ' + communityCards[0]);
+        disCommunityCard2.setAttribute('class', 'card ' + communityCards[1]);
+        disCommunityCard3.setAttribute('class', 'card ' + communityCards[2]);
+    }
+
+    if (round >= 3) {
+        disCommunityCard4.setAttribute('class', 'card ' + communityCards[3]);
+    }
+    
+    if (round >= 4) {
+        disCommunityCard5.setAttribute('class', 'card ' + communityCards[4]);
+    }
+}
+
+function renderPlayerHand() {
+    disPlayerCard1.setAttribute('class', 'card ' + playerHand[0]);
+    disPlayerCard2.setAttribute('class', 'card ' + playerHand[1]);
+}
+
+function renderMessage () {
+    disMessage.innerText = message;
+}
+
+function renderEmmettHand () {
+    disEmmettCard1.setAttribute('class', 'card ' + emmettHand[0]);
+    disEmmettCard2.setAttribute('class', 'card ' + emmettHand[1]);}
+
+//render community cards: if round 1 render 3, if round2 render 4, etc
+function compareHands() {
+
 }
 
 function shuffleDeck() {
