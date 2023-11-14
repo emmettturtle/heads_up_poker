b1c3edbfc0154979ea3023c3338eb893da325ac8
Emmett Verba <emmettverba@gmail.com>
2023-11-14 10:44:11 -0700

    adds cached DOM elements and adds the message to the HTML and CSS

--- js/main.js
+++ js/main.js
@@ -34,6 +34,21 @@ let playerCurrBet;
 /*----- cached elements  -----*/
 const disPlayerBet = document.getElementById('player-bet-value');
 const disEmmettBet = document.getElementById('emmett-bet-value');
+const disPot = document.getElementById('pot-value');
+const disPlayerCard1 = document.getElementById('player-card1');
+const disPlayerCard2 = document.getElementById('player-card2');
+const disEmmettCard1 = document.getElementById('emmett-card1');
+const disEmmettCard2 = document.getElementById('emmett-card2');
+const disPlayerBank = document.getElementById('player-bank-value');
+const disEmmettBank = document.getElementById('emmett-bank-value');
+const disCommunityCard1 = document.getElementById('card1');
+const disCommunityCard2 = document.getElementById('card2');
+const disCommunityCard3 = document.getElementById('card3');
+const disCommunityCard4 = document.getElementById('card4');
+const disCommunityCard5 = document.getElementById('card5');
+const disMessage = document.getElementById('message');
+
+
 
 /*----- event listeners -----*/
 
